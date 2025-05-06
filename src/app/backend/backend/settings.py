4add913a4fa52d85d8b53cd8570cc00b8550bd79
TeHoang <tehoang18@gmail.com>
2025-05-06 19:01:37 +0700

    add requirements, fix setting

--- src/app/backend/backend/settings.py
+++ src/app/backend/backend/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'django_filters',
     'corsheaders'
 ]
 
@@ -101,8 +102,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'football_management',
+        'USER': 'postgres', 
+        'PASSWORD': 'realpassword123',  
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
